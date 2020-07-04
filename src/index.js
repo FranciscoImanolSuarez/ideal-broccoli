f33632c63e0356030e1783cb2d5d322f7ef0775c
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2020-07-04 15:44:31 -0300

    Change name

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const messages = ["Imanol", "Ana", "Oscar", "Jessica", "Diego"];
+const messages = ["The best broccoli in the world"];
 
 const randomMsg = () => {
   const msj = messages[Math.floor(Math.random() * messages.length)];
