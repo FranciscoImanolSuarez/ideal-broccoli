b2a6408b95f42bfbfe7633752a4e0743bb18ef33
Nick Barrz <43194506+FernandoBarrz@users.noreply.github.com>
2020-07-17 09:29:03 -0500

    Add 3 phrases to messages in index.js
    Change indentation of "messages" array

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-const messages = ["The best broccoli in the world", "Reinventing the wheel", "🐱‍🚀","🐱‍🏍 "];
+const messages = [
+  "The best broccoli in the world",
+  "Reinventing the wheel",
+  "🐱‍🚀",
+  "🐱‍🏍 ",
+  "\"The best way to predict the future is to create it.\" - Peter Drucker",
+  "\"Controlling complexity is the essence of computer programming.\" - Brian Kernighan",
+  "\"Computers are useless. They can only give you answers.\" - Pablo Picasso",
+];
 
 const randomMsg = () => {
   const msj = messages[Math.floor(Math.random() * messages.length)];
